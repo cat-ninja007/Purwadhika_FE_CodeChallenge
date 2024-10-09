9062faba06fde5fef1e75ad7b6c1c8ee17ccb779
cat-ninja007 <archiedesign12.2@gmail.com>
2024-10-09 14:16:13 +0700

    Initialize project - stuck add tailwind

---
+++ src/components/Header/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import { FC } from "react";
+
+
+const Header:FC = () => {
+    return (
+      <header className="bg-blue-500 p-4 text-white">
+        <h1 className="text-xl">Courtney Lanka</h1>
+      </header>
+    );
+  };
+
+  export default Header;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,48 @@
-import type { Config } from "tailwindcss";
+// import type { Config } from "tailwindcss";
+
+// const config: Config = {
+//   content: [
+//     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         background: "var(--background)",
+//         foreground: "var(--foreground)",
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
+// export default config;
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./app/**/*.{js,ts,jsx,tsx}", // Path untuk App Router
+//     "./pages/**/*.{js,ts,jsx,tsx}",
+//     "./components/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './app/**/*.{js,ts,jsx,tsx}', // untuk aplikasi Next.js dengan App Router
+    './pages/**/*.{js,ts,jsx,tsx}', // untuk halaman biasa
+    './components/**/*.{js,ts,jsx,tsx}', // untuk komponen
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
+
 export default config;
